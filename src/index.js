ae3ec477b8981b95031579f3715c30b36b238380
Armin Blaze <box_for_spam@mail.ru>
2019-06-25 17:46:36 +0300

    106 - Reducer

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+
+const initialState = 0;
+
+const reducer = (state = 0, action = {}) => {
+
+  if(action.type === 'INC') {
+    return state + 1;
+  }
+
+  return state;
+}
+
+let newState = reducer(undefined, {});
+// let newState = reducer(initialState, {type: 'INC'});
+
+console.log(newState);
