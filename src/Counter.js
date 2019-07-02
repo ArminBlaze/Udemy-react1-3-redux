b1f0cb6ff13aef70652ffa79e4f058977c9d7e59
Armin Blaze <box_for_spam@mail.ru>
2019-07-02 23:48:50 +0300

    test2

--- src/Counter.js
+++ src/Counter.js
@@ -4,9 +4,9 @@ const Counter = () => {
   return (
     <React.Fragment> 
       <h2>0</h2>
-      <button className="btn btn-primary btn-lg">-</button>
-      <button className="btn btn-primary btn-lg">+</button>
-      <button className="btn btn-primary btn-lg">Random</button>
+      <button className="btn btn-primary btn-lg">-</button>{' '}
+      <button className="btn btn-primary btn-lg">+</button>{' '}
+      <button className="btn btn-primary btn-lg">Random</button>{' '}
     </React.Fragment>
   )
 }
