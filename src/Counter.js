5acae261e6bf32294d3712ad90082dddca85500c
Armin Blaze <box_for_spam@mail.ru>
2019-07-03 23:30:45 +0300

    114 - React + Redux (без react-redux)

--- src/Counter.js
+++ src/Counter.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Counter = () => {
+const Counter = ({counter, inc, dec, rnd}) => {
   return (
     <React.Fragment> 
-      <h2>0</h2>
-      <button className="btn btn-primary btn-lg">-</button>{' '}
-      <button className="btn btn-primary btn-lg">+</button>{' '}
-      <button className="btn btn-primary btn-lg">Random</button>{' '}
+      <h2>{counter}</h2>
+      <button onClick={dec} className="btn btn-primary btn-lg">-</button>{' '}
+      <button onClick={inc} className="btn btn-primary btn-lg">+</button>{' '}
+      <button onClick={rnd} className="btn btn-primary btn-lg">Random</button>{' '}
     </React.Fragment>
   )
 }
--- src/index.js
+++ src/index.js
@@ -13,33 +13,24 @@ const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
 
 store.subscribe(update);
 
+update();
 
-// const counter = document.getElementById('counter');
-
-// document
-//   .getElementById('increment')
-//   .addEventListener('click', inc);
-
-// document
-//   .getElementById('decrement')
-//   .addEventListener('click', dec);
-
-// document
-//   .getElementById('random')
-//   .addEventListener('click', () => { 
-//     const value = Math.floor( Math.random() * 10 ) + 1;
-//     console.log('Random: ' + value)
-//     rnd(value);
-//   });
-  
   
 function update () {
-  // console.log( store.getState() );
-  // counter.innerHTML = store.getState()
+  ReactDOM.render(<Counter 
+    counter={ store.getState() }
+    inc={ inc }
+    dec={ dec }
+    rnd={ () => { 
+      const value = Math.floor( Math.random() * 10 ) + 1;
+      console.log('Random: ' + value)
+      rnd(value);
+    } }
+    />,
+    document.getElementById('root'));
 }
 
 
 
 
-ReactDOM.render(<Counter />,
-  document.getElementById('root'));
+
